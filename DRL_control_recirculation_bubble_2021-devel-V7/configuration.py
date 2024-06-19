2bcc05895a9dd9a0f3397a30a6d6d48109d16414
Quelthezar <pieter@orlandinimusic.com>
2024-06-19 13:39:47 +0200

    Added DRL coded from Pol (formatted by blackd)

--- DRL_control_recirculation_bubble_2021-devel-V7/configuration.py
+++ DRL_control_recirculation_bubble_2021-devel-V7/configuration.py
@@ -12,28 +12,34 @@ import os
 
 
 ## PATHS
-DRL_PATH  = os.path.join(os.path.dirname(os.path.abspath(__file__))) # Absolute path to the DRL directory
-ALYA_PATH = os.path.join(DRL_PATH,'alya_files')                      # Absolute path to the alya_files folder
-BIN_PATH  = os.path.join(DRL_PATH,'alya_files','bin')                # Absolute path to the binaries folder
+DRL_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__))
+)  # Absolute path to the DRL directory
+ALYA_PATH = os.path.join(
+    DRL_PATH, "alya_files"
+)  # Absolute path to the alya_files folder
+BIN_PATH = os.path.join(
+    DRL_PATH, "alya_files", "bin"
+)  # Absolute path to the binaries folder
 
 
 ## FILE NAMES
-NODELIST   = 'nodelist'
+NODELIST = "nodelist"
 
 
 ## BINARIES
-ALYA_BIN   = os.path.join(BIN_PATH,'alya_3D_longfunc.x')
-ALYA_SETS  = os.path.join(BIN_PATH,'alya-sets')
-ALYA_CLEAN = os.path.join(BIN_PATH,'alya-clean')
-ALYA_ULTCL = os.path.join(BIN_PATH,'ultra_cleaner.sh')
-ALYA_GMSH  = os.path.join(BIN_PATH,'pyalya_gmsh2alya')
-ALYA_PERIO = os.path.join(BIN_PATH,'pyalya_periodic')
-ALYA_INCON = os.path.join(BIN_PATH,'pyalya_initialcondition')
+ALYA_BIN = os.path.join(BIN_PATH, "alya_3D_longfunc.x")
+ALYA_SETS = os.path.join(BIN_PATH, "alya-sets")
+ALYA_CLEAN = os.path.join(BIN_PATH, "alya-clean")
+ALYA_ULTCL = os.path.join(BIN_PATH, "ultra_cleaner.sh")
+ALYA_GMSH = os.path.join(BIN_PATH, "pyalya_gmsh2alya")
+ALYA_PERIO = os.path.join(BIN_PATH, "pyalya_periodic")
+ALYA_INCON = os.path.join(BIN_PATH, "pyalya_initialcondition")
 
 
 ## OPTIONS
 # to use dardel ---> true
-USE_SLURM = True
+USE_SLURM = False
 
 
 # Oversubscribe over MPI, must be set to False always
@@ -42,9 +48,9 @@ USE_SLURM = True
 OVERSUBSCRIBE = True
 
 
-# Activate the debug mode, which means you get prints of every action 
+# Activate the debug mode, which means you get prints of every action
 # and every action saves a log, that you can find in (enviroment1-->EP_1-->logs)
 
-#It is recommended to use the debug mode with 1 enviroment and 1 proc, 
-#in order not to mix prints from different enviroments
+# It is recommended to use the debug mode with 1 enviroment and 1 proc,
+# in order not to mix prints from different enviroments
 DEBUG = False
