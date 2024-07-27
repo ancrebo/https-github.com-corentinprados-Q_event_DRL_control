a6af979b9f4173639d6f1c88db5266f19e884f7f
Pieter Orlandini <pietero@kth.se>
2024-07-27 14:19:11 +0200

    TESTING: adjusting witness visualization

--- DRL_POL/visualization.py
+++ DRL_POL/visualization.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 from typing import List, Tuple
 
 from logging_config import configure_logger, DEFAULT_LOGGING_LEVEL
@@ -31,65 +33,62 @@ def plot_witness_points(
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
 
-    # Adjust orientation so that X-Z plane is horizontal and Y is vertical
-    x_vals = [coord[0] * nx_Qs for coord in coordinates]
-    y_vals = [coord[1] for coord in coordinates]
-    z_vals = [coord[2] * nz_Qs for coord in coordinates]
+    # Create a color map for different local volumes
+    colors = plt.cm.get_cmap("tab20", nx_Qs * nz_Qs)
 
-    # FOR VISUALIZATION ONLY, Y AND Z ARE SWAPPED - Pieter
-    ax.scatter(x_vals, z_vals, y_vals, c="r", marker="o")
+    # Adjust orientation so that X-Z plane is horizontal and Y is vertical
+    x_vals = np.array([coord[0] for coord in coordinates])
+    y_vals = np.array([coord[1] for coord in coordinates])
+    z_vals = np.array([coord[2] for coord in coordinates])
+
+    # Plot points for each local volume separately
+    for i in range(nx_Qs):
+        for j in range(nz_Qs):
+            # Define the local volume boundaries
+            x_min = i / nx_Qs
+            x_max = (i + 1) / nx_Qs
+            z_min = j / nz_Qs
+            z_max = (j + 1) / nz_Qs
+
+            # Filter points within this local volume
+            volume_filter = (
+                (x_vals >= x_min)
+                & (x_vals < x_max)
+                & (z_vals >= z_min)
+                & (z_vals < z_max)
+            )
+            ax.scatter(
+                x_vals[volume_filter],
+                z_vals[volume_filter],
+                y_vals[volume_filter],
+                color=colors(i * nz_Qs + j),
+                marker="o",
+                label=f"Volume ({i}, {j})",
+            )
 
     ax.set_xlabel("X")
     ax.set_ylabel("Z")
     ax.set_zlabel("Y")
     ax.set_title("3D Plot of Witness Points")
 
-    # Setting the grid based on the number of agents
-    ax.set_xticks([i for i in range(nx_Qs + 1)])
-    ax.set_yticks([i for i in range(nz_Qs + 1)])
+    # Setting the grid and tick values based on the number of agents
+    ax.set_xticks([i + 0.5 for i in range(nx_Qs)])
+    ax.set_xticklabels([str(i + 1) for i in range(nx_Qs)])
+    ax.set_yticks([i + 0.5 for i in range(nz_Qs)])
+    ax.set_yticklabels([str(i + 1) for i in range(nz_Qs)])
 
     # Show "z" ticks based on y_value_density, but only show every y_skip_values
     z_tick_indices = [i for i in range(1, y_value_density + 1, y_skip_values)]
-    ax.set_zticks([i / y_value_density for i in z_tick_indices])
+    ax.set_zticks([i for i in z_tick_indices])
     ax.set_zticklabels([str(i) for i in z_tick_indices])
 
-    # Highlighting the (0, 0) indexed volume
-    step_x = 1 / nx_Qs
-    step_z = 1 / nz_Qs
-    volume_outline = [
-        # Bottom face
-        [0, step_x, step_x, 0, 0, 0],
-        [0, 0, step_z, step_z, 0, 0],
-        [0, 0, 0, 0, 0, 0],  # y values for bottom face
-        # Top face
-        [0, step_x, step_x, 0, 0, 0],
-        [0, 0, step_z, step_z, 0, 0],
-        [1, 1, 1, 1, 1, 1],  # y values for top face
-        # Vertical lines
-        [0, 0, step_x, step_x, step_x, step_x, 0, 0],
-        [0, 0, 0, 0, step_z, step_z, step_z, step_z],
-        [0, 1, 0, 1, 0, 1, 0, 1],  # y values for vertical lines
-    ]
-
-    for i in range(0, len(volume_outline[0]), 2):
-        ax.plot(
-            volume_outline[0][i : i + 2],
-            volume_outline[1][i : i + 2],
-            volume_outline[2][i : i + 2],
-            color="blue",
-            linestyle="dotted",
-        )
-
-    for i in range(4, len(volume_outline[0]), 2):
-        ax.plot(
-            volume_outline[0][i : i + 2],
-            volume_outline[1][i : i + 2],
-            volume_outline[2][i : i + 2],
-            color="blue",
-            linestyle="dotted",
-        )
+    # Set limits to ensure the ticks are correctly displayed
+    ax.set_xlim(0, nx_Qs)
+    ax.set_ylim(0, nz_Qs)
+    ax.set_zlim(0, y_value_density)
 
     ax.grid(True)
+    ax.legend(loc="upper right")
 
     plt.savefig(filename)
     plt.close(fig)
