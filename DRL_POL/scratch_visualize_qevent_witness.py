ad30460f7961e428662451e4de0c4cb312b1ecfd
Pieter Orlandini <pietero@kth.se>
2024-07-31 17:18:00 +0200

    TESTING: trying to add Q event bool to VTK as scalar field and saving

--- DRL_POL/scratch_visualize_qevent_witness.py
+++ DRL_POL/scratch_visualize_qevent_witness.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
 
     # Save the modified mesh to a new VTK file
     modified_path = os.path.join(
-        save_directory, f"EP_{episode}_timestep_{timestep_to_load}_Q_event.vtu"
+        path, f"{episode}_timestep_{timestep_to_load}_Q_event.vtu"
     )
     logger.info("Saving the modified mesh to %s", modified_path)
 
